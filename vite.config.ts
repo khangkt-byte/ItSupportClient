c4ddad3c47c4ef9d8c70ddfd798efc45957426b7
khangkt <188341984+khangkt-byte@users.noreply.github.com>
2026-02-10 17:04:12 +0700

    migration Tailwind CSS v4

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
-    basicSsl()
+    basicSsl(),
+    tailwindcss()
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
